8ffe433d95647499bc50a35cf9ad2e86c4cb3dc3
F4bioo <costa.fbo@gmail.com>
2023-04-14 20:09:08 -0300

    Initial commit

---
+++ app/src/main/java/com/fappslab/aptoide/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fappslab.aptoide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
